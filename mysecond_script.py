59f87dfdec24be8031ae76b14728aa3c00a0be94
TagaySergey1968 <72980403+TagaySergey1968@users.noreply.github.com>
2020-10-17 17:59:08 +0300

    Add files via upload

---
+++ mysecond_script.py
@@ -0,0 +1,9 @@
+name="Henry"
+height=200
+weight=80
+bmi=(height*0.4)
+print("индекс массы тела"+str(bmi))
+if bmi<(height*0.4):
+	print ("У "+name+" нет лишнего веса")
+else
+	print ("У "+name+" имеется лишний вес")
